8ddd3c62bcf5499ea1f40e84b2106eb4abefdddd
Fernando Hernández <ferhd@yahoo.com>
2024-04-29 11:46:24 -0600

    Uso de excepciones por entrada no válida.

--- com/aluracursos/conversorDeMonedas/Principal/Principal.java
+++ com/aluracursos/conversorDeMonedas/Principal/Principal.java
@@ -20,71 +20,76 @@ public class Principal {
     String key1 = "USD";
     String key2 = "USD";
 
-    while (true) {
-      System.out.println(Menu.stars);
-      System.out.println(Menu.legend + "\n");
-      System.out.println(Menu.menu);
-      System.out.println(Menu.stars);
-      var opcion = userInput.nextInt();
-      if (opcion == 9) {
-        userInput.close();
-        break;
-      }
-      if (opcion == 1 || opcion == 3 || opcion == 5) {
-        moneda = monedaUSA;
-      } else if (opcion == 2) {
-        monedaExRateAPI = monedaAPI.getMoneda("ARS");
-        Moneda monedaARS = new Moneda(monedaExRateAPI);
-        moneda = monedaARS;
-      } else if (opcion == 4) {
-        monedaExRateAPI = monedaAPI.getMoneda("BRL");
-        Moneda monedaBRL = new Moneda(monedaExRateAPI);
-        moneda = monedaBRL;
-      } else if (opcion == 6) {
-        monedaExRateAPI = monedaAPI.getMoneda("COP");
-        Moneda monedaCOP = new Moneda(monedaExRateAPI);
-        moneda = monedaCOP;
-      } else {
-        continue; // Volvemos a presentar el menú
-      }
-      conversion_rates = moneda.getConversion_rates();
-      System.out.println("Ingrese el valor que desea convertir:");
-      var valor = userInput.nextDouble();
+    try {
+      while (true) {
+        System.out.println(Menu.stars);
+        System.out.println(Menu.legend + "\n");
+        System.out.println(Menu.menu);
+        System.out.println(Menu.stars);
+        var opcion = userInput.nextInt();
+        
+        if (opcion == 9) {
+          userInput.close();
+          break;
+        }
+        if (opcion == 1 || opcion == 3 || opcion == 5) {
+          moneda = monedaUSA;
+        } else if (opcion == 2) {
+          monedaExRateAPI = monedaAPI.getMoneda("ARS");
+          Moneda monedaARS = new Moneda(monedaExRateAPI);
+          moneda = monedaARS;
+        } else if (opcion == 4) {
+          monedaExRateAPI = monedaAPI.getMoneda("BRL");
+          Moneda monedaBRL = new Moneda(monedaExRateAPI);
+          moneda = monedaBRL;
+        } else if (opcion == 6) {
+          monedaExRateAPI = monedaAPI.getMoneda("COP");
+          Moneda monedaCOP = new Moneda(monedaExRateAPI);
+          moneda = monedaCOP;
+        } else {
+          continue; // Volvemos a presentar el menú
+        }
+        conversion_rates = moneda.getConversion_rates();
+        System.out.println("Ingrese el valor que desea convertir:");
+        var valor = userInput.nextDouble();
 
-      switch (opcion) {
-        case 1:
-          key1 = "USD";
-          key2 = "ARS";
-          break;
-        case 2:
-          key1 = "ARS";
-          key2 = "USD";
-          break;
-        case 3:
-          key1 = "USD";
-          key2 = "BRL";
-          break;
-        case 4:
-          key1 = "BRL";
-          key2 = "USD";
-          break;
-        case 5:
-          key1 = "USD";
-          key2 = "COP";
-          break;
-        case 6:
-          key1 = "COP";
-          key2 = "USD";
-          break;
-        default:
-          key1 = "USD";
-          key2 = "USD";
-          break;
+        switch (opcion) {
+          case 1:
+            key1 = "USD";
+            key2 = "ARS";
+            break;
+          case 2:
+            key1 = "ARS";
+            key2 = "USD";
+            break;
+          case 3:
+            key1 = "USD";
+            key2 = "BRL";
+            break;
+          case 4:
+            key1 = "BRL";
+            key2 = "USD";
+            break;
+          case 5:
+            key1 = "USD";
+            key2 = "COP";
+            break;
+          case 6:
+            key1 = "COP";
+            key2 = "USD";
+            break;
+          default:
+            key1 = "USD";
+            key2 = "USD";
+            break;
+        }
+        valorFinal = valor * conversion_rates.get(key2);
+        System.out.println(String.format("El valor %.2f [%s] corresponde"
+                          + " al valor final de =>> %.2f [%s]\n", 
+                          valor, key1,valorFinal, key2));
       }
-      valorFinal = valor * conversion_rates.get(key2);
-      System.out.println(String.format("El valor %.2f [%s] corresponde"
-                        + " al valor final de =>> %.2f [%s]\n", 
-                        valor, key1,valorFinal, key2));
+    } catch (Exception e) {
+      System.out.println("Opción no válida, solo números por favor");
     }
     System.out.println("Finalizó la ejecución del programa!");        
   }
